74370279c8a193297d53839061e940304c7b6346
Bymba-code <bymba6607@gmail.com>
2024-11-12 15:32:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express")
+const cors = require("cors")
 const {testDatabaseConnection } = require("./DATABASE")
 const userRoutes = require("./Routes/USER/index")
 const authRoutes = require("./Routes/AUTH/index")
 const categoryRoutes = require("./Routes/CATEGORY")
 const productRoutes = require("./Routes/PRODUCT/index")
 
+const corsOptions = {
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+    credential: true,
+}
+
+
+
 const app = express()
 
 app.use(express.json())
+app.use(cors(corsOptions));
+
 
 testDatabaseConnection();
 
@@ -21,4 +33,4 @@ app.use("/api/v1",  categoryRoutes)
 
 app.use("/api/v1", productRoutes)
 
-app.listen(3000, console.log("App listening 3000"))
+app.listen(5000, console.log("App listening 5000"))
