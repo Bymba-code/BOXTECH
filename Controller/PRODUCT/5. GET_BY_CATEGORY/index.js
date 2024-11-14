4d66fc1be034790f3dcd2b9a2062f06474a04455
Bymba-code <bymba6607@gmail.com>
2024-11-14 19:19:44 +0800

    Update index.js

--- Controller/PRODUCT/5. GET_BY_CATEGORY/index.js
+++ Controller/PRODUCT/5. GET_BY_CATEGORY/index.js
@@ -2,21 +2,17 @@ const { executeQuery } = require("../../../DATABASE");
 
 const GET_BY_CATEGORY = async (req, res) => {
     try {
+        
         const { categoryName } = req.params;
 
+        const {page, limit } = req.query;
 
-        // Query to get paginated products
+        const offset = (page - 1) * limit
+
         const query = `
-            SELECT p.id, p.product_name, p.price, p.category_name, AVG(pr.rating) AS rating
-            FROM products p
-            LEFT JOIN product_rating pr ON p.id = pr.product_id
-            WHERE p.category_name = ?
-            GROUP BY p.id, p.product_name, p.price, p.category_name
-            ORDER BY p.product_name
+            SELECT * from products limit ? offset ?
         `;
-        
-        const data = await executeQuery(query, [categoryName]);
-      
+        const data = await executeQuery(query)
             if (data.length === 0) {
             return res.status(404).json({
                 success: false,
