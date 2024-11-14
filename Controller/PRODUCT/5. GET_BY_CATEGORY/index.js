3e3b1d8d7bb2ec0eb29d995e6f11cb843c4b4917
Bymba-code <bymba6607@gmail.com>
2024-11-14 19:53:35 +0800

    Update index.js

--- Controller/PRODUCT/5. GET_BY_CATEGORY/index.js
+++ Controller/PRODUCT/5. GET_BY_CATEGORY/index.js
@@ -29,7 +29,7 @@ const GET_BY_CATEGORY = async (req, res) => {
         const query = "SELECT p.id, p.product_name, p.price, p.category_name, AVG(pr.rating) AS rating FROM products p LEFT JOIN product_rating pr ON p.id = pr.product_id WHERE p.category_name = ? GROUP BY p.id, p.product_name, p.price, p.category_name ORDER BY p.product_name LIMIT ? OFFSET ?"
 
         // Execute the query with the category name, limit, and offset
-        const data = await executeQuery(query, [categoryName, limit, page]);
+        const data = await executeQuery(query, [categoryName, limit, offset]);
 
         if (data.length === 0) {
             return res.status(404).json({
