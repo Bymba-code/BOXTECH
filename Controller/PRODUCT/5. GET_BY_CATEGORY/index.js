14b99b05896ae05cf7a4bd771e4fcadb147aa921
Bymba-code <bymba6607@gmail.com>
2024-11-14 19:43:50 +0800

    Update index.js

--- Controller/PRODUCT/5. GET_BY_CATEGORY/index.js
+++ Controller/PRODUCT/5. GET_BY_CATEGORY/index.js
@@ -15,6 +15,7 @@ const GET_BY_CATEGORY = async (req, res) => {
 
         const maxPages = Math.ceil(totalCount / limit);
 
+         page = Math.min(Math.max(page, 1), maxPages)
         
         // Construct the query
         const query = "SELECT p.id, p.product_name, p.price, p.category_name, AVG(pr.rating) AS rating FROM products p LEFT JOIN product_rating pr ON p.id = pr.product_id WHERE p.category_name = ? GROUP BY p.id, p.product_name, p.price, p.category_name ORDER BY p.product_name LIMIT ? OFFSET ?"
