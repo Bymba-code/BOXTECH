3e540cdddc02c32cc9a437adb61196c3dc41822a
Bymba-code <bymba6607@gmail.com>
2024-11-19 18:07:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'localhost:3000', // Your React app's URL
+    origin: 'http://localhost:3000', // Your React app's URL
     credentials: true,
 }))
 app.use(cookieParser())
