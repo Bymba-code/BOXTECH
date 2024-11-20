e63005c40aecf28df5454c82eea9f69d4e9170db
Bymba-code <bymba6607@gmail.com>
2024-11-20 17:40:37 +0800

    Update index.js

--- Routes/Checkout/index.js
+++ Routes/Checkout/index.js
@@ -1,17 +1,15 @@
 const express = require("express")
 const GET_ALL_CHECKOUT = require("../../Controller/7. CHECKOUT_CONTROLLER/1. GET_ALL")
-const authenticateToken = require("../../Middlewares/Authenticate")
-const authorizeRoles = require("../../Middlewares/Authorization")
 const GET_USER_CHECKOUT = require("../../Controller/7. CHECKOUT_CONTROLLER/2. GET_USERS_CHECKOUT")
 const CREATE_CHECKOUT = require("../../Controller/7. CHECKOUT_CONTROLLER/3. CREATE_CHECKOUT")
 
 const router = express.Router()
 
 router.route("/checkout")
-.get(authenticateToken, authorizeRoles("admin") ,GET_ALL_CHECKOUT)
-.post(authenticateToken, authorizeRoles("admin", "user"), CREATE_CHECKOUT)
+.get(GET_ALL_CHECKOUT)
+.post(CREATE_CHECKOUT)
 
-router.route("/checkout/:id").get(authenticateToken, authorizeRoles("user", "admin"), GET_USER_CHECKOUT)
+router.route("/checkout/:id").get(GET_USER_CHECKOUT)
 
 
 module.exports = router
