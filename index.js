41a13fba3bce606a5632f47037e40b9871b58202
Bymba-code <bymba6607@gmail.com>
2024-11-20 17:46:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://omn1club.com"],
+    origin: ["http://localhost:3000"],
     credentials: true
 }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
