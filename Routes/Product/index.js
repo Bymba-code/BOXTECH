65ac884964870a87e126be8ff73e6743b9aaaa92
Bymba-code <bymba6607@gmail.com>
2024-11-20 17:43:32 +0800

    Update index.js

--- Routes/Product/index.js
+++ Routes/Product/index.js
@@ -3,6 +3,7 @@ const GET_ALL_PRODUCT = require("../../Controller/1. PRODUCT_CONTROLLER/1. GET_A
 const GET_ALL_BY_CATEGORY_PRODUCT = require("../../Controller/1. PRODUCT_CONTROLLER/2. GET_BY_CATEGORY")
 const GET_SINGLE_PRODUCT = require("../../Controller/1. PRODUCT_CONTROLLER/3. GET_SINGLE")
 const POST_CREATE_PRODUCT = require("../../Controller/1. PRODUCT_CONTROLLER/4. CREATE_PRODUCT")
+const GET_USER_PRODUCT = require("../../Controller/8. USER_PRODUCT_CONTROLLER/1. GET_ALL")
 
 const router = express.Router()
 
@@ -12,5 +13,7 @@ router.route("/product").get(GET_ALL_PRODUCT)
 router.route("/product/:category").get(GET_ALL_BY_CATEGORY_PRODUCT)
 
 router.route("/product/single/:id").get(GET_SINGLE_PRODUCT)
+
+router.route("/product/user/:id").get(GET_USER_PRODUCT)
 
 module.exports = router
