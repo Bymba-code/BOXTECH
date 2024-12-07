d84a26fa5e2aa77eb66475b8c5ecf12594a1b5b8
Bymba-code <bymba6607@gmail.com>
2024-12-07 18:08:53 +0800

    Update index.js

--- Controller/10. ADMIN_CONTROLLER/4. CATEGORY/index.js
+++ Controller/10. ADMIN_CONTROLLER/4. CATEGORY/index.js
@@ -3,6 +3,9 @@ const {executeQuery} = require("../../../DATABASE/index")
 const CATEGORY_ADMIN = async (req, res) => {
     try 
     {
+        const page = parseInt(req.query.page, 10) || 1; // Default to page 1
+        const limit = parseInt(req.query.limit, 10) || 10; // Default to 10 items per page
+        const offset = (page - 1) * limit; // Calculate offset
        const query = `SELECT 
                 c.id AS category_id,
                 c.name AS category_name,
@@ -15,19 +18,28 @@ const CATEGORY_ADMIN = async (req, res) => {
             LEFT JOIN 
                 product_sell ps ON ps.product = p.id
             GROUP BY 
-                c.id, c.name;
+                c.id, c.name
+            LIMIT ${limit} OFFSET ${offset};
 `
     const data = await executeQuery(query)
     
-    if(data)
-    {
-        return res.status(200).json({
-            success:true,
-            data:data,
-            message: "Амжилттай"
-        })
-    }
+            const countQuery = `
+            SELECT COUNT(*) AS total
+            FROM categories;
+        `;
+        const totalCountResult = await executeQuery(countQuery);
+        const totalCount = totalCountResult[0]?.total || 0;
 
+        if (data) {
+            return res.status(200).json({
+                success: true,
+                data: data,
+                currentPage: page,
+                totalPages: Math.ceil(totalCount / limit),
+                totalItems: totalCount,
+                message: "Амжилттай",
+            });
+        }
     
     }
     catch(err)
