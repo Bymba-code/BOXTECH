5e34d0f709327af274ec9915f2294d53a4c7f563
Bymba-code <bymba6607@gmail.com>
2024-12-09 03:24:52 +0800

    Update index.js

--- Routes/Admin/index.js
+++ Routes/Admin/index.js
@@ -5,6 +5,9 @@ const PRODUCT_ADMIN = require("../../Controller/10. ADMIN_CONTROLLER/3.PRODUCT")
 const authenticateToken = require("../../Middlewares/Authenticate")
 const authorizeRoles = require("../../Middlewares/Authorization")
 const CATEGORY_ADMIN = require("../../Controller/10. ADMIN_CONTROLLER/4. CATEGORY")
+const USERS_ADMIN = require("../../Controller/10. ADMIN_CONTROLLER/6.USERS")
+const DELETE_USER = require("../../Controller/10. ADMIN_CONTROLLER/7. USER_DELETE")
+const UPDATE_USER = require("../../Controller/10. ADMIN_CONTROLLER/8. USER_UPDATE")
 
 const router = express.Router()
 
@@ -15,4 +18,9 @@ router.route("/admin/recent").get(authenticateToken, authorizeRoles("admin"),REC
 router.route("/admin/products").get(authenticateToken, authorizeRoles("admin"),PRODUCT_ADMIN)
 
 router.route("/admin/category").get(CATEGORY_ADMIN)
+
+router.route("/admin/users").get(USERS_ADMIN).post(DELETE_USER)
+
+router.route("/admin/update/users").post(UPDATE_USER)
+
 module.exports = router
