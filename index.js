1f34f7e6560632965399ae6b3db957c63348355b
Bymba-code <bymba6607@gmail.com>
2025-01-03 18:38:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
   
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://yourdomain.com"],
+    origin: ["http://localhost:5173", "https://omn1club.com"],
     methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
     credentials: true, 
