e28cc5c2ad5e8110024c8602a8680867b311c23f
Bymba-code <bymba6607@gmail.com>
2025-01-07 19:06:16 +0800

    Update index.js

--- Routes/11. News/index.js
+++ Routes/11. News/index.js
@@ -5,6 +5,9 @@ const Authenticate = require("../../Middlewares/Authenticate")
 const Authorize = require("../../Middlewares/Authorization")
 const INSERT_NEWS = require("../../Controllers/12. NEWS_CATEGORY/2. INSERT")
 const GET_SINGLE_NEWS = require("../../Controllers/11. NEWS_CONTROLLER/2. GET_SINGLE")
+const COMMENT_NEWS = require("../../Controllers/11. NEWS_CONTROLLER/5. COMMENT")
+const GET_COMMENT_NEWS = require("../../Controllers/13. NEWS_COMMENT/1. GET_ALL")
+const CREATE_COMMENT_NEWS = require("../../Controllers/13. NEWS_COMMENT/2. INSERT")
 
 const router = express.Router()
 
@@ -13,5 +16,10 @@ router.route("/news").get(GET_NEWS).post(Authenticate, Authorize(["admin", "user
 router.route("/news/single/:id").get(GET_SINGLE_NEWS)
 
 router.route("/news/category").get(NEWS_CATEGORY)
+
+
+router.route("/news/comment").post(Authenticate, CREATE_COMMENT_NEWS)
+
+router.route("/news/comment/:id").get(GET_COMMENT_NEWS)
 
 module.exports = router
