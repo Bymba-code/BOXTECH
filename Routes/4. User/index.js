2cbcb8a4a0605c1225f592a97b1d1e09de7d7bfa
Bymba-code <bymba6607@gmail.com>
2025-01-10 05:48:56 +0800

    Update index.js

--- Routes/4. User/index.js
+++ Routes/4. User/index.js
@@ -4,6 +4,7 @@ const Authorize = require("../../Middlewares/Authorization")
 const INSERT_BANK = require("../../Controllers/4. USER_CONTROLLER/1. INSERT_BANK")
 const UPDATE_BANK = require("../../Controllers/4. USER_CONTROLLER/2. UPDATE_BANK")
 const DELETE_BANK = require("../../Controllers/4. USER_CONTROLLER/3. DELETE_BANK")
+const USER_PRODUCT = require("../../Controllers/4. USER_CONTROLLER/4. USER_PRODUCTS")
 
 const router = express.Router()
 
@@ -12,6 +13,7 @@ router.route("/bank/account")
 .put(Authenticate, Authorize(["user"]), UPDATE_BANK)
 .delete(Authenticate, Authorize(["user"]), DELETE_BANK)
 
+router.route("/users/products").get(Authenticate, USER_PRODUCT)
 
 
 module.exports = router
