a64e05ff9c6d51c9b7e339a34c08ebc4b6ffab8b
Bymba-code <bymba6607@gmail.com>
2025-01-10 02:32:57 +0800

    Update index.js

--- Controllers/3. PRODUCT_CONTROLLER/6. USER/index.js
+++ Controllers/3. PRODUCT_CONTROLLER/6. USER/index.js
@@ -3,12 +3,58 @@ const {executeQuery} = require("../../../Database/test")
 const GET_USER_PRODUCT = async (req, res) => {
     try 
     {
-        const query = `SELECT * FROM user_products WHERE user = ?`
-        const data = await executeQuery(query, [req.user.id])
+        const {page, size} = req.query;
+
+        const pageNumber = parseInt(page, 10);
+        const pageSize = parseInt(size, 10);
+
+        if (isNaN(pageNumber) || isNaN(pageSize) || pageNumber <= 0 || pageSize <= 0) {
+            return res.status(400).json({
+              success: false,
+              data: [],
+              message: "Хуудасны эсвэл хэмжээг зөв оруулна уу.",
+            });
+        }
+        
+        const countQuery = `
+        SELECT COUNT(*) as total_count
+        FROM products
+        WHERE products.user = ?
+        `;
+            const countData = await executeQuery(countQuery, [req.user.id]);
+            const totalCount = countData[0]?.total_count || 0;
+            
+            // Step 2: Calculate maxPage
+            const maxPage = Math.ceil(totalCount / pageSize);
+
+        const offset = (pageNumber - 1) * pageSize;
+
+        const query =   `SELECT 
+                        products.*,
+                        category.name AS category_name,
+                        AVG(product_rating.rating) AS average_rating,
+                        COUNT(product_reviews.id) AS review_count,
+                        SUM(deposit_history.deposit) AS total_deposit_amount
+                        FROM
+                        products
+                        LEFT JOIN category 
+                        ON products.category = category.id
+                        LEFT JOIN product_rating 
+                        ON products.id = product_rating.product
+                        LEFT JOIN product_reviews 
+                        ON products.id = product_reviews.product
+                        LEFT JOIN deposit_history 
+                        ON products.id = deposit_history.product
+                        WHERE products.user = ?
+                        GROUP BY 
+                        products.id, category.id
+                        LIMIT ? OFFSET ?`
+       
+        const data = await executeQuery(query, [req.user.id, size.toString(), offset.toString()])
+
         return res.status(200).json({
-            success:true,
-            data: data,
-            message: "Амжилттай"
+            data:data,
+            maxPage:maxPage
         })
     }
     catch(err)
