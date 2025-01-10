523c800fe70875e642d14c36db97c13d8eb78552
Bymba-code <bymba6607@gmail.com>
2025-01-10 17:22:16 +0800

    Update index.js

--- Routes/12. Profile/index.js
+++ Routes/12. Profile/index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const PROFILE_HOME = require("../../Controllers/13. PROFILE_CONTROLLER/1. HOME")
 const Authenticate = require("../../Middlewares/Authenticate")
+const PROFILE_HOME_DETAIL = require("../../Controllers/13. PROFILE_CONTROLLER/2. PROFILE")
 
 const router = express.Router()
 
 
 router.route("/profile").get(Authenticate, PROFILE_HOME)
+
+router.route("/profile/home").get(Authenticate, PROFILE_HOME_DETAIL)
 
 module.exports = router
