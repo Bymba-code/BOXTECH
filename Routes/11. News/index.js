10964eebf296ad5c957b318a1edb37b73f3f9ec7
Bymba-code <bymba6607@gmail.com>
2025-01-10 17:48:52 +0800

    Update index.js

--- Routes/11. News/index.js
+++ Routes/11. News/index.js
@@ -8,6 +8,7 @@ const GET_SINGLE_NEWS = require("../../Controllers/11. NEWS_CONTROLLER/2. GET_SI
 const COMMENT_NEWS = require("../../Controllers/11. NEWS_CONTROLLER/5. COMMENT")
 const GET_COMMENT_NEWS = require("../../Controllers/13. NEWS_COMMENT/1. GET_ALL")
 const CREATE_COMMENT_NEWS = require("../../Controllers/13. NEWS_COMMENT/2. INSERT")
+const INSERT_REVIEWS_NEWS = require("../../Controllers/11. NEWS_CONTROLLER/6. REVIEWS")
 
 const router = express.Router()
 
@@ -21,5 +22,8 @@ router.route("/news/category").get(NEWS_CATEGORY)
 router.route("/news/comment").post(Authenticate, CREATE_COMMENT_NEWS)
 
 router.route("/news/comment/:id").get(GET_COMMENT_NEWS)
+
+
+router.route("/reviews/news/:id").get(INSERT_REVIEWS_NEWS)
 
 module.exports = router
