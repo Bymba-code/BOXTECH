01c59a98837e686a6a5fa46956dae77a2f192652
Bymba-code <bymba6607@gmail.com>
2025-01-13 14:52:26 +0800

    Update index.js

--- Routes/16. Users/index.js
+++ Routes/16. Users/index.js
@@ -5,6 +5,8 @@ const INSERT_USERS = require("../../Controllers/16. USERS/3. INSERT")
 const Authenticate = require("../../Middlewares/Authenticate")
 const Authorize = require("../../Middlewares/Authorization")
 const DASHBOARD = require("../../Controllers/17. DASHBOARD_CONTROLLER/DASHBOARD")
+const DASHBOARD_PROFILE = require("../../Controllers/17. DASHBOARD_CONTROLLER/2. PROFILE")
+
 
 const router = express.Router()
 
@@ -14,5 +16,8 @@ router.route("/users")
 .post(Authenticate, Authorize(["admin"]), INSERT_USERS)
 
 router.route("/stats").get(DASHBOARD)
+
+router.route("/user/stats").get(Authenticate, DASHBOARD_PROFILE)
+
 
 module.exports = router
