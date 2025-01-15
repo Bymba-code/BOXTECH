d71623f0f8f4e3c92fc6642c50b2d4e3842e8a00
Bymba-code <bymba6607@gmail.com>
2025-01-16 06:16:26 +0800

    Update index.js

--- Controllers/PRODUCT_CONTROLLER_V2/2. DOWNLOAD/index.js
+++ Controllers/PRODUCT_CONTROLLER_V2/2. DOWNLOAD/index.js
@@ -1,70 +1,66 @@
-const {executeQuery} = require("../../../Database/test")
+const { executeQuery } = require("../../../Database/test");
 const path = require('path');
 const fs = require('fs');
 
 const downloadFile = async (req, res) => {
-    try 
-    {
+    try {
         const { id } = req.params; 
 
-        // 1. Файл мэдээлэл авах
-        const queryOne = `SELECT * FROM products WHERE id = ?`
-    
-        const productDB = await executeQuery(queryOne, [id])
-        const product = productDB[0]
-    
-        // 2. Файл татаж авах эрх шалгах
-        const queryTwo = `SELECT * FROM user_products WHERE user = ? AND product = ? LIMIT 1`
-    
-        const userPermission = await executeQuery(queryTwo, [req.user.id , product.id]) 
-        const permissionDate = userPermission[0]
-    
-        if(!permissionDate)
-        {
-            return res.status(404).json({
-                success:false,
+
+        const queryOne = `SELECT * FROM products WHERE id = ?`;
+        const productDB = await executeQuery(queryOne, [id]);
+        const product = productDB[0];
+
+        const queryTwo = `SELECT * FROM user_products WHERE user = ? AND product = ? LIMIT 1`;
+        const userPermission = await executeQuery(queryTwo, [req.user.id, product.id]);
+        const permissionDate = userPermission[0];
+
+        if (!permissionDate) {
+            return res.status(402).json({
+                success: false,
                 data: [],
-                message: "Та файлыг татаж авхын тулд худалдаж авна уу"
-            })
-        }
-    
-        const today = new Date()
-    
-        if(permissionDate.date <= today)
-        {
-            return res.status(403).json({
-                success:false,
-                data: [],
-                message: "Файлыг татаж авах хугацаа дууссан байна."
-            })
-        }
-    
-    
-        const filePath = path.join(__dirname, '../../../uploads/files', product.file.split("/")[3]);
-    
-        if (fs.existsSync(filePath)) {
-            res.download(filePath, (err) => {
-                if (err) {
-                    return res.status(500).json({
-                        success: false,
-                        message: 'Татаж авах явцад ямар нэгэн алдаа гарлаа'
-                    });
-                }
+                message: "Та файлыг худалдаж авна уу."
             });
+        }
+
+        const today = new Date();
+
+        if (permissionDate.date <= today) {
+            return res.status(403).json({
+                success: false,
+                data: [],
+                message: "Таны файлыг татаж авах хугацаа дууссан байна."
+            });
+        }
+        else 
+        {
+            
+        const filePath = path.join(__dirname, '../../../uploads/files', product.file.split("/")[3]);
+        const fileName = product.file.split("/").pop(); 
+
+
+        if (fs.existsSync(filePath)) {
+            res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+            res.setHeader('Content-Type', 'application/octet-stream');  
+
+            const fileStream = fs.createReadStream(filePath);
+            fileStream.pipe(res);
+            
+            
         } else {
             return res.status(404).json({
                 success: false,
-                message: 'Файл устарсан эсвэл байхгүй байна.'
+                message: 'File has expired or does not exist.'
             });
         }
-    }
-    catch(err)
-    {
+        }
+    } catch (err) {
+        console.error(err);  
         return res.status(500).json({
-            success:false,
-            data:[],
-            message: "Серверийн алдаа гарлаа"
-        })
+            success: false,
+            data: [],
+            message: "A server error occurred."
+        });
     }
 };
 
