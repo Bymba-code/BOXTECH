4b1267821ab142dd1513da0284b3b2ab39dbc869
Bymba-code <bymba6607@gmail.com>
2025-01-16 17:35:22 +0800

    Update index.js

--- Controllers/PRODUCT_CONTROLLER_V2/2. DOWNLOAD/index.js
+++ Controllers/PRODUCT_CONTROLLER_V2/2. DOWNLOAD/index.js
@@ -1,11 +1,6 @@
-const { executeQuery } = require("../../../Database/test");
-const path = require('path');
-const fs = require('fs');
-
 const downloadFile = async (req, res) => {
     try {
         const { id } = req.params; 
-
 
         const queryOne = `SELECT * FROM products WHERE id = ?`;
         const productDB = await executeQuery(queryOne, [id]);
@@ -24,38 +19,53 @@ const downloadFile = async (req, res) => {
         }
 
         const today = new Date();
-
-        if (permissionDate.date <= today) {
+        if (new Date(permissionDate.date) <= today) {
             return res.status(403).json({
                 success: false,
                 data: [],
                 message: "Таны файлыг татаж авах хугацаа дууссан байна."
             });
         }
-        else 
-        {
-            
+        
         const filePath = path.join(__dirname, '../../../uploads/files', product.file.split("/")[3]);
-        const fileName = product.file.split("/").pop(); 
-
+        const fileName = product.file.split("/").pop();
 
         if (fs.existsSync(filePath)) {
-            res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
-            res.setHeader('Content-Type', 'application/octet-stream');  
+            const fileStats = fs.statSync(filePath);
+            const fileSize = fileStats.size;
+            const range = req.headers.range;
+            
+            if (range) {
+                const [start, end] = range
+                    .replace(/bytes=/, "")
+                    .split("-")
+                    .map(Number);
 
-            const fileStream = fs.createReadStream(filePath);
-            fileStream.pipe(res);
-            
-            
+                const chunkSize = end - start + 1;
+                const readStream = fs.createReadStream(filePath, { start, end });
+
+                res.status(206);
+                res.setHeader("Content-Range", `bytes ${start}-${end}/${fileSize}`);
+                res.setHeader("Accept-Ranges", "bytes");
+                res.setHeader("Content-Length", chunkSize);
+                res.setHeader("Content-Type", "application/octet-stream");
+
+                readStream.pipe(res);
+            } else {
+                res.setHeader("Content-Disposition", `attachment; filename="${fileName}"`);
+                res.setHeader("Content-Type", "application/octet-stream");  
+
+                const fileStream = fs.createReadStream(filePath);
+                fileStream.pipe(res);
+            }
         } else {
             return res.status(404).json({
                 success: false,
                 message: 'File has expired or does not exist.'
             });
         }
-        }
     } catch (err) {
-        console.error(err);  
+        console.error(err);
         return res.status(500).json({
             success: false,
             data: [],
@@ -63,5 +73,3 @@ const downloadFile = async (req, res) => {
         });
     }
 };
-
-module.exports = { downloadFile };
