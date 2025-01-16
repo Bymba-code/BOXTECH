582b8e2870fc3a3c7e05678a184ac55a09023103
Bymba-code <bymba6607@gmail.com>
2025-01-16 18:26:18 +0800

    Update index.js

--- Controllers/PRODUCT_CONTROLLER_V2/2. DOWNLOAD/index.js
+++ Controllers/PRODUCT_CONTROLLER_V2/2. DOWNLOAD/index.js
@@ -18,54 +18,55 @@ const downloadFile = async (req, res) => {
         // 2. Токен задлах
         const accessToken = jwt.verify(token, process.env.TOKEN_SECRET)
         
+        if(accessToken.role === "user")
+        {
+            // 3. Файл татах эрхийг шалгах
+            const queryTwo = "SELECT * FROM user_products WHERE user = ? and product = ?"
+            const dataTwo = await executeQuery(queryTwo , [accessToken.id, id])
 
-        // 3. Файл татах эрхийг шалгах
-        const queryTwo = "SELECT * FROM user_products WHERE user = ? and product = ?"
-        const dataTwo = await executeQuery(queryTwo , [accessToken.id, id])
+            if (dataTwo.length === 0) {
+                // If the user hasn't purchased the file, return an HTML page informing them
+                const htmlContent = `
+                    <!DOCTYPE html>
+                    <html lang="mn">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Файл Худалдаж Авах</title>
+                    </head>
+                    <body>
+                        <h1>Файлыг Худалдаж Авна Уу</h1>
+                        <p>Та энэ файлыг татаж авахын тулд худалдаж авах хэрэгтэй.</p>
+                        <a href="/boxtech/store">Файл худалдаж авах</a>
+                    </body>
+                    </html>
+                `;
+                return res.status(404).send(htmlContent);
+            }
 
-        if (dataTwo.length === 0) {
-            // If the user hasn't purchased the file, return an HTML page informing them
-            const htmlContent = `
-                <!DOCTYPE html>
-                <html lang="mn">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Файл Худалдаж Авах</title>
-                </head>
-                <body>
-                    <h1>Файлыг Худалдаж Авна Уу</h1>
-                    <p>Та энэ файлыг татаж авахын тулд худалдаж авах хэрэгтэй.</p>
-                    <a href="/boxtech/store">Файл худалдаж авах</a>
-                </body>
-                </html>
-            `;
-            return res.status(404).send(htmlContent);
+            const date = new Date();
+
+            if (dataTwo[0].date < date) {
+                const htmlContent = `
+                    <!DOCTYPE html>
+                    <html lang="mn">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Файл Татаж Авах Хугацаа Дууссан</title>
+                    </head>
+                    <body>
+                        <h1>Таны файлыг татаж авах хугацаа дууссан байна.</h1>
+                        <p>Та дахин худалдан авсны дараа файлыг татаж авах боломжтой.</p>
+                        <a href="/boxtech/store">Дахин Худалдаж Авах</a>
+                    </body>
+                    </html>
+                `;
+                return res.status(401).send(htmlContent);
+            }
+
         }
 
-        const date = new Date();
-
-        if (dataTwo[0].date < date) {
-            const htmlContent = `
-                <!DOCTYPE html>
-                <html lang="mn">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Файл Татаж Авах Хугацаа Дууссан</title>
-                </head>
-                <body>
-                    <h1>Таны файлыг татаж авах хугацаа дууссан байна.</h1>
-                    <p>Та дахин худалдан авсны дараа файлыг татаж авах боломжтой.</p>
-                    <a href="/boxtech/store">Дахин Худалдаж Авах</a>
-                </body>
-                </html>
-            `;
-            return res.status(401).send(htmlContent);
-        }
-
-
-        
         const filePath = path.join(__dirname, '../../../uploads/files', product.file.split("/")[3]);
     
         if (fs.existsSync(filePath)) {
