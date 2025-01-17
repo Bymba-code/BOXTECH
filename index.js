3ec7ee1ec621f1dc3b8a1c90cedae5bd35bc1397
Bymba-code <bymba6607@gmail.com>
2025-01-17 19:32:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const usersRoutes = require("./Routes/16. Users")
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
   
 const corsOptions = {
     origin: ["http://localhost:5173", "https://omn1club.com", "https://boxtech.mn"],
@@ -31,6 +30,9 @@ const corsOptions = {
     allowedHeaders: ["Content-Type", "Authorization"], 
     credentials: true, 
 };
+
+app.use(express.json({ limit: '50gb' }))
+app.use(express.urlencoded({ limit: '50gb', extended: true })); 
 
 app.use(cors(corsOptions));
 
